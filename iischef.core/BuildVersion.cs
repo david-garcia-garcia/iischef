6e5492f3e8b7bb03b0b6384f2c82dfa43330fb59
Davvid <deivid.garcia.garcia@gmail.com>
2024-02-12 15:34:53 +0100

    Fixes to initial commit

---
+++ iischef.core/BuildVersion.cs
@@ -0,0 +1,10 @@
+﻿namespace iischef.core
+{
+    public static class BuildVersion
+    {
+        /// <summary>
+        /// Build version replaced at build time in IC
+        /// </summary>
+        public const string BUILDVERSION = "{BUILDVERSION}";
+    }
+}
